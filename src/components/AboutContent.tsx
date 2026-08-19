422e5700683176fbc48ea04451af1ae6e34a139a
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-19 20:11:01 +0700

    fix: remove twitter from AboutContent and update connect section

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Fragment } from "react"; // <-- TAMBAHIN IMPORT INI
 import { aboutConfig } from "@/config/about";
 
 export default function AboutContent() {
