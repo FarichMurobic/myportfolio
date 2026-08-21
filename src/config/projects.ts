cc0988b5d56521f050f1626402839e4c15134478
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-21 17:05:46 +0700

    add: projects

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -21,13 +21,13 @@ export const projectsConfig = {
       description: "Share details about your third project. What problems did it solve? What was the outcome?",
       href: "#!",
       imageUrl: "/assets/images/projects/project3.png"
+    },
+    {
+      title: "Personal Portfolio Website",
+      description: "Personal portfolio website built with Next.js 14 and Tailwind CSS. Showcasing my experience as a Junior Java Developer, including projects, certifications, and blog posts. Fully responsive with dark mode support.",
+      href: "#!",
+      imageUrl: "/assets/images/projects/project1.jpg"
     }
-//     {
-//       title: "Personal Portfolio Website",
-//       description: "Personal portfolio website built with Next.js 14 and Tailwind CSS. Showcasing my experience as a Junior Java Developer, including projects, certifications, and blog posts. Fully responsive with dark mode support.",
-//       href: "#!",
-//       imageUrl: "/assets/images/projects/project1.jpg"
-//     },
 //     {
 //       title: "Java Fundamentals & Object-Oriented Programming",
 //       description: "A comprehensive learning journey covering Java fundamentals, Object-Oriented Programming (OOP), Collections Framework, Exception Handling, File I/O, Generics, Multithreading, Data Structures, Algorithms, and software development best practices.",
