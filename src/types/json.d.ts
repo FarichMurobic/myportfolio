bb29d3073cffdafaf3d8beee82badcddf099de2a
Farich Murobic <farichmurobiq11@gmail.com>
2026-08-30 19:29:40 +0700

    Update types/json.d.ts

--- src/types/json.d.ts
+++ src/types/json.d.ts
@@ -1,4 +1,13 @@
+/**
+ * TypeScript Declarations - Portfolio
+ * @author Farich Murobic
+ * @email farichmurobiq11@gmail.com
+ * @github https://github.com/FarichMurobic
+ * @website https://farichmurobic.vercel.app
+ */
+
+// Enable importing JSON files as modules in TypeScript
 declare module "*.json" {
   const value: any;
   export default value;
-} 
+}
